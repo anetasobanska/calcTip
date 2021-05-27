e2b1395d909c645b5c69257ba706fa2ac9a011a5
anetasobanska <sannettka@gmail.com>
2021-05-27 09:34:43 +0200

    refactoring

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-const bills = [100, 1000, 40];
-const tips = [];
-
-function calcTip(bill){
-    return (bill > 50 && bill < 300) ? tips.push(bill * 0.15) : tips.push(bill * 0.20)
+const calcTip = function(bill){
+    return (bill > 50 && bill < 300) ? (bill * 0.15) : (bill * 0.20)
 }
 
-calcTip(bills[0]);
-calcTip(bills[1]);
-calcTip(bills[2]);
+const bills = [100, 1000, 40];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totalBills = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 
-console.log(tips);
+console.log(tips, bills);
+console.log(totalBills);
